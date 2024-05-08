11ca5e8076e32c7e6a2f6b383e69e3a365ce6194
Paulo Prates <paulo.prates01@hotmail.com>
2024-05-07 21:27:27 -0300

    aula sobre tipos primitivos em ts com exemplos práticos

---
+++ typescript-matheus-battisti/2-fundamentos/tipos-primitivos/index.js
@@ -0,0 +1,23 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof (x));
+var y = 15.4345;
+console.log(typeof (y));
+console.log(y);
+console.log(y.toPrecision(3));
+// 2 - string
+var FIRST_NAME = "Paulo";
+console.log(FIRST_NAME.toUpperCase());
+var fullName;
+var LAST_NAME = "Prates";
+fullName = FIRST_NAME + " " + LAST_NAME;
+console.log(fullName);
+console.log(typeof (fullName));
+// 3 - boolean
+var a = false;
+console.log(a);
+console.log(typeof (a));
+a = true;
+console.log(a);
---
+++ typescript-matheus-battisti/2-fundamentos/tipos-primitivos/index.ts
@@ -0,0 +1,44 @@
+// 1 - numbers
+
+let x: number = 10;
+
+console.log(x);
+
+x = 16;
+
+console.log(typeof(x));
+
+const y: number = 15.4345;
+
+console.log(typeof(y));
+
+console.log(y);
+
+console.log(y.toPrecision(3));
+
+// 2 - string
+
+const FIRST_NAME: string = "Paulo";
+
+console.log(FIRST_NAME.toUpperCase());
+
+let fullName: string;
+
+const LAST_NAME: string = "Prates";
+
+fullName = FIRST_NAME + " " + LAST_NAME;
+
+console.log(fullName);
+
+console.log(typeof(fullName));
+
+// 3 - boolean
+
+let a: boolean = false;
+
+console.log(a);
+console.log(typeof(a));
+
+a = true;
+
+console.log(a);
