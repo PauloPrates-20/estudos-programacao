fad6488ca2f440d4eaa44b6beae12968f46659d3
Paulo Prates <paulo.prates01@hotmail.com>
2024-05-07 22:09:47 -0300

    finalização da seção 2 - fundamentos de ts e resolução do desafio2

---
+++ typescript-matheus-battisti/2-fundamentos/desafio2/index.js
@@ -0,0 +1,3 @@
+var x = 10;
+var stringX = x.toString();
+console.log("O número é " + stringX);
---
+++ typescript-matheus-battisti/2-fundamentos/desafio2/index.ts
@@ -0,0 +1,4 @@
+let x: number = 10;
+let stringX = x.toString();
+
+console.log("O número é " + stringX);
