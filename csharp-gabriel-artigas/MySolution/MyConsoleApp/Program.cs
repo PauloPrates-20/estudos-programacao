bff66feae5c7f7afbc58f702ca950903b5289562
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-20 16:21:33 -0300

    Aula: Comentários de código fonte

---
+++ csharp-gabriel-artigas/MySolution/MyConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        /*
+         * Esta é uma aplicação de teste,
+         * sendo usada apenas para fins de teste.
+         * Onde estamos testando linhas e
+         * Blocos de comentários
+         */
+        static void Main(string[] args)
+        {
+            // Escreve a mensagem "Olá, Mundo!"
+            Console.WriteLine("Primeiro Comando");
+            Console.WriteLine("Segundo Comando");
+            //Console.WriteLine("Terceiro Comando");
+            Console.WriteLine("Quarto Comando");
+        }
+    }
+}
