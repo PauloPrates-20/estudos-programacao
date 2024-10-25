c97ab98070db5b29c340d553450a694ac45e7d5a
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-25 14:18:45 -0300

    Aula: Métodos de Entrada e Saída de Dados em C#

---
+++ csharp-gabriel-artigas/EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Paulo");
+            //Console.Write("Prates");
+
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+    }
+}
