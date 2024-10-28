43d9a08aa460b7ba5f21f517b1ead828b6a47e2c
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-28 18:19:44 -0300

    Aula: Conversão implicita de tipos numéricos

---
+++ csharp-gabriel-artigas/Conversao/Conversao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 100;
+            ushort num2;
+
+            float num3 = 1250.45f;
+            double num4 = num3;
+
+            int numero = 'C';
+
+            Console.WriteLine(numero);
+            Console.ReadKey();
+        }
+    }
+}
