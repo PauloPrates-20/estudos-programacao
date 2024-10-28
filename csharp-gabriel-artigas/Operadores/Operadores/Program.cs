b60763849d339b823610ad2e288075f3f32556e9
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-28 18:49:31 -0300

    Aula: Operadores de incremento e decremento

--- csharp-gabriel-artigas/Operadores/Operadores/Program.cs
+++ csharp-gabriel-artigas/Operadores/Operadores/Program.cs
@@ -16,13 +16,24 @@ namespace Operadores
             //int resultado = num1 + num2;
             #endregion
 
-            double num1 = 100;
-            double num2 = 10;
-            double num3 = 5;
+            #region Precedência de Operadores Aritméticos
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
 
-            double res = (num1 + num2) * (num3 / 3);
+            //double res = (num1 + num2) * (num3 / 3);
+            #endregion
 
-            Console.WriteLine(res);
+            int num1 = 10;
+
+            // num1 = num1 + 1;
+            // ++num1;
+            // num1++;
+            // --num1;
+            // num1--;
+
+            Console.WriteLine(num1++);
+            Console.WriteLine(num1);
         }
     }
 }
