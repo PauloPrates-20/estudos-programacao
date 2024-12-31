b9cfc09a1108b644044bbfd9c999be1b25a5e081
Paulo Prates <paulo.prates01@hotmail.com>
2024-12-31 17:58:02 -0300

    Seção 9 - Estruturas Condicionais

---
+++ csharp-gabriel-artigas/CalcularIMC/CalcularIMC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o peso em Kg: ");
+            float peso = float.Parse(Console.ReadLine());
+            Console.Write("Informe sua altura em metros: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            float imc = (float)(peso / Math.Pow(altura, 2));
+
+            Console.Write($"\nIMC = {imc} -> ");
+            if (imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso.");
+            }
+            else if (imc < 25)
+            {
+                Console.WriteLine("Normal.");
+            }
+            else if (imc < 30)
+            {
+                Console.WriteLine("Acima do peso.");
+            }
+            else if (imc < 35)
+            {
+                Console.WriteLine("Obeso.");
+            }
+            else
+            {
+                Console.WriteLine("Muito obeso.");
+            }
+        }
+    }
+}
---
+++ csharp-gabriel-artigas/Condicao/Condicao/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Estruturas Condicionais Simples
+            //int valor = 5;
+
+            //if (valor == 5)
+            //{
+            //    Console.WriteLine("Condição verdadeira");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Conidação falsa");
+            //}
+            #endregion
+
+            #region Estruturas Condicionais Encadeadas
+            //int valor = 5;
+
+            //if (valor < 5)
+            //{
+            //    Console.WriteLine("Condição verdadeira");
+            //}
+            //else if (valor < 10)
+            //{
+            //    Console.WriteLine("Condição alternativa 1");
+            //}
+            //else if (valor < 20)
+            //{
+            //    Console.WriteLine("Condição alternativa 2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Condição falsa");
+            //}
+            #endregion
+
+            #region Estruturas Condicionais Aninhadas
+            //int numero = 10;
+
+            //if (numero > 5)
+            //{
+            //    Console.Write("O número é maior que 5");
+
+            //    if (numero % 2 == 0)
+            //    {
+            //        Console.WriteLine(" e também é par");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" mas não é par.");
+            //    }
+            //}
+            #endregion
+
+            #region Operador Ternário
+            int numero = 10;
+            string mensagem = "";
+
+            // condicao ? true : false;
+            mensagem = numero > 5 ? "Maior que 5" : "Menor que 5";
+
+            Console.WriteLine(mensagem);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
