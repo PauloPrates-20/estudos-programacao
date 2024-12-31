19734f49414e8b866177197867e898d938dc2c7f
Paulo Prates <paulo.prates01@hotmail.com>
2024-12-31 18:03:39 -0300

    Aula 46 - Estrutura de controle (switch - case)

---
+++ csharp-gabriel-artigas/Controle/Controle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 1;
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("Caso 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Caso 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Caso 3");
+                    break;
+                default:
+                    Console.WriteLine("Nenhum caso");
+                    break;
+            }
+        }
+    }
+}
