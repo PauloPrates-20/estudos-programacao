61588cef68e00cbea712620ae19f04a0670f6134
Paulo Prates <paulo.prates01@hotmail.com>
2024-12-31 19:02:33 -0300

    Aula 53 - Exercício Sequência de Fibonnaci

---
+++ csharp-gabriel-artigas/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 1 1 2 3 5 8
+            int a = 0, b = 1, c = 0;
+
+            Console.Write("Quantos valores imprimir: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sequência de Fibonnaci com {valores} valores");
+
+            for (int i = 0; i  < valores; i++)
+            {
+                Console.Write(i == valores - 1 ? Convert.ToString(a) : (a + ", "));
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
