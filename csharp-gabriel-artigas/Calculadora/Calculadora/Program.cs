8f6b190a4a98fcdc4ec532b894ce940d2a034e23
Paulo Prates <paulo.prates01@hotmail.com>
2024-12-31 18:30:02 -0300

    Aula 48 - Exercício Calculadora

---
+++ csharp-gabriel-artigas/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicio:
+            Console.Write("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.Write("Escolha a operação (+ - x /): ");
+            char op = char.Parse(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (op)
+            {
+                default:
+                    Console.WriteLine("Operação inválida.");
+                    goto Inicio;
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case 'x':
+                case 'X':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Impossível dividir por 0\n");
+                        goto Inicio;
+                    }
+
+                    resultado = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine($"O resultado da operação é: {resultado}");
+        }
+    }
+}
