cf3ff24bfe0e0e67947dd35f832852a50758a3d7
Paulo Prates <paulo.prates01@hotmail.com>
2025-02-14 13:04:26 -0300

    Seção 13 - Express com TypeScript

--- typescript-matheus-battisti/13-express-com-ts/src/app.ts
+++ typescript-matheus-battisti/13-express-com-ts/src/app.ts
@@ -2,7 +2,7 @@
 // console.log('Express com TS!!!');
 
 // 2 - init express
-import express, { Request, Response} from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 const app = express();
 
@@ -38,10 +38,6 @@ app.get('/api/interfaces', (req: Request, res: Response) => {
   res.send('Utilizando as interfaces');
 });
 
-app.listen(3000, () => {
-  console.log('Aplicação TS + Express rodando!');
-});
-
 // 6 - enviando json
 app.get('/api/json', (req: Request, res: Response) => {
   res.json({
@@ -51,3 +47,91 @@ app.get('/api/json', (req: Request, res: Response) => {
     sizes: ['P', 'M', 'G']
   });
 });
+
+// 7 - router parameters
+app.get('api/product/:id', (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const id = req.params.id;
+
+  if (id === '1') {
+    const product = {
+      id: 1,
+      name: 'Boné',
+      price: 10
+    };
+
+    res.json(product)
+  } else {
+    res.send('Produto não encontrado.');
+  }
+});
+
+// 8 - rotas complexas
+app.get('api/products/:productId/reviews/:reviewId', (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const productId = req.params.productId;
+  const reviewId = req.params.reviewId;
+
+  res.send(`Acessando a review ${reviewId} do produto ${productId}`);
+});
+
+// 9 - router handler
+function getUser(req: Request, res: Response) {
+  console.log(`Resgatando o usuário com o id: ${req.params.id}`);
+
+  res.send('Usuário encontrado.')
+}
+
+// 10 - middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === '1') {
+    console.log('Pode seguir');
+    next();
+  }
+  else {
+    console.log('acesso restrito');
+  }
+}
+
+// 11 - middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log(req.path);
+  next();
+}
+
+app.use(showPath);
+
+app.get('/api/user/:id/access', checkUser, (req: Request, res: Response) => {
+  res.json({ msg: 'Bem vindo à área administrativa.' });
+});
+
+app.get('/api/user/:id', getUser);
+
+// 12 - req e res com generics
+app.get('/api/user/:id/details/:name',
+  (
+    req: Request<{ id: string, name: string }>,
+    res: Response<{ status: boolean }>
+  ) => {
+    console.log(`ID: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
+
+    res.json({ status: true });
+  });
+
+// 13 - tratamento de erros
+app.get('/api/erro', (req: Request, res: Response) => {
+  try {
+    // nossa lógica
+    throw new Error('Algo deu errado');
+  } catch (error: any) {
+    res.status(500).json({ msg: error.message });
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Aplicação TS + Express rodando!');
+});
+
