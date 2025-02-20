7d0b639bb02e8e042a946690d3967ff623baf8c6
Paulo Prates <paulo.prates01@hotmail.com>
2025-02-20 13:03:51 -0300

    Seção 14 - API RESTful com TypeScript

--- typescript-matheus-battisti/14-ts-api-restful/src/controllers/movieControllers.ts
+++ typescript-matheus-battisti/14-ts-api-restful/src/controllers/movieControllers.ts
@@ -10,6 +10,7 @@ export async function createMovie(req: Request, res: Response) {
     res.status(201).json(movie);
   } catch (e: any) {
     logger.error(`Erro no sistema: ${e.message}`);
+    res.status(500).json({ error: 'Por favor, tente novamente mais tarde.'});
   }
 }
 
@@ -20,10 +21,59 @@ export async function findMovieById(req: Request, res: Response) {
 
     if (!movie) {
       res.status(404).json({ error: "O filme não existe" });
+      return;
     }
 
     res.status(200).json(movie);
   } catch (e: any) {
     logger.error(`Erro no sistema: ${e.message}`);
+    res.status(500).json({ error: 'Por favor, tente novamente mais tarde.'});
+  }
+}
+
+export async function getAllMovies(req: Request, res: Response) {
+  try {
+    const movies = await MovieModel.find();
+    res.status(200).json(movies);
+  } catch (e: any) {
+    logger.error(`Erro no sistema: ${e.message}`);
+    res.status(500).json({ error: 'Por favor, tente novamente mais tarde.'});
+  }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      res.status(404).json({ error: 'O filme não existe' });
+      return;
+    }
+
+    await movie.deleteOne();
+    res.status(200).json({ msg: 'Filme removido com sucesso' });
+  } catch (e: any) {
+    logger.error(`Erro no sistema: ${e.message}`);
+    res.status(500).json({ error: 'Por favor, tente novamente mais tarde.'});
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    let movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      res.status(404).json({ error: 'O filme não existe' });
+      return;
+    }
+
+    await MovieModel.updateOne({ _id: id }, data);
+    res.status(200).json({ data });
+  } catch (e: any) {
+    logger.error(`Erro no sistema: ${e.message}`);
+    res.status(500).json({ error: 'Por favor, tente novamente mais tarde.'});
   }
 }
--- typescript-matheus-battisti/14-ts-api-restful/src/router.ts
+++ typescript-matheus-battisti/14-ts-api-restful/src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express"
-import { createMovie, findMovieById } from "./controllers/movieControllers"
+import { createMovie, findMovieById, getAllMovies, removeMovie, updateMovie } from "./controllers/movieControllers"
 import { validate } from './middleware/handleValidation'
 import { movieCreateValidation } from "./middleware/movieValidation";
 
@@ -10,4 +10,7 @@ export default router
     res.status(200).send('API working!');
   })
   .post('/movie', movieCreateValidation(), validate, createMovie)
+  .get('/movie', getAllMovies)
   .get('/movie/:id', findMovieById)
+  .delete('/movie/:id', removeMovie)
+  .patch('/movie/:id', movieCreateValidation(), validate, updateMovie)
